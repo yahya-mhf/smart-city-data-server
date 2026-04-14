5582821e6b16a86674f252b779c437f3d6d3a449
yahya-mhf <68018817+yahya-mhf@users.noreply.github.com>
2026-04-14 22:04:17 +0100

    start

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, Data SERVER")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	fmt.Println("Server running at http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
